8909587ed68019ae2d4cab70bdce5250a13a2305
Oleg Polyakov <olegpolyakov@outlook.com>
2019-09-24 23:10:54 +0300

    Add includes

--- index.php
+++ index.php
@@ -21,52 +21,9 @@ $utm_content = isset($_GET['utm_content']) ? $_GET['utm_content'] : null;
 
     <link rel="stylesheet" href="./css/index.css">
 
-    <!-- Facebook Pixel -->
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-
-        fbq('init', '1798029673849756');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1798029673849756&amp;ev=PageView&amp;noscript=1"></noscript>
-    <!-- /Facebook Pixel -->
-
-    <!-- Yandex.Metrika -->
-    <script type="text/javascript" >
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    ym(50571868, "init", {
-        id:50571868,
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-    });
-    </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/50571868" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-    <!-- /Yandex.Metrika -->
-
-    <!-- Google.Analytics -->
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-47133492-2', 'sayes.ru');
-        ga('require', 'displayfeatures');
-        ga('send', 'pageview');
-    </script>
-    <!-- /Google.Analytics -->
+    <?php include './includes/facebook-pixel.php' ?>
+    <?php include './includes/google-analytics.php' ?>
+    <?php include './includes/yandex-metrika.php' ?>
 </head>
 
 <body class="page<?= isset($target) ? ' target-'.$target : '' ?>">
--- thank-you.php
+++ thank-you.php
@@ -7,58 +7,13 @@
     <meta name="yandex-verification" content="77618a975ea21450">
     <link rel="icon" href="/img/favicon-32x32.png" sizes="32x32">
     <link rel="icon" href="/img/favicon-192x192.png" sizes="192x192">
+    <link rel="stylesheet" href="/css/index.css">
 
     <title>Спасибо за заявку! | SAY YES!</title>
 
-    <link rel="stylesheet" href="/css/index.css">
-
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    ym(53050876, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-    });
-    </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/53050876" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-    <!-- /Yandex.Metrika counter -->
-
-    <!-- Facebook Pixel -->
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-
-        fbq('init', '1798029673849756');
-        fbq('track', 'PageView');
-        fbq('track', 'Lead');
-        fbq('track', 'SubmitApplication');
-    </script>
-    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1798029673849756&amp;ev=PageView&amp;noscript=1"></noscript>
-    <!-- /Facebook Pixel -->
-
-    <!-- Google.Analytics -->
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-47133492-2', 'sayes.ru');
-        ga('require', 'displayfeatures');
-        ga('send', 'pageview');
-    </script>
-    <!-- /Google.Analytics -->
+    <?php include './includes/facebook-pixel.php' ?>
+    <?php include './includes/google-analytics.php' ?>
+    <?php include './includes/yandex-metrika.php' ?>
 </head>
 <body>
     <main id="thank-you" class="hero">
